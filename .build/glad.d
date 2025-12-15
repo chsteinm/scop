1e8d3b84eed8f2d8a5595ba1092bf6181c8be53e
Christophe Steinmetz <chrstein@z1r6p4.42lyon.fr>
2025-12-15 23:05:30 +0100

    buildRemoved

--- .build/glad.d
+++
@@ -1,3 +0,0 @@
-.build/glad.o: .deps/glad.c .deps/glad/glad.h .deps/KHR/khrplatform.h
-.deps/glad/glad.h:
-.deps/KHR/khrplatform.h:
--- .build/main.d
+++
@@ -1,3 +0,0 @@
-.build/main.o: main.cpp .deps/glad/glad.h .deps/KHR/khrplatform.h
-.deps/glad/glad.h:
-.deps/KHR/khrplatform.h:
